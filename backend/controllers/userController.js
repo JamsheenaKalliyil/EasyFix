419d528b422e887af01a22a66348468205791d16
jamsheena <jamsheenalezin8@gmail.com>
2026-08-08 18:54:41 +0530

    Fix production authentication cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,8 +70,8 @@ const userController = {
 
       res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       });
 
