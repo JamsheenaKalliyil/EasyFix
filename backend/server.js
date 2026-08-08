dc78c9346e619b29b79c2771dc37a6df91b89df3
jamsheena <jamsheenalezin8@gmail.com>
2026-08-08 16:10:01 +0530

    Configure frontend CORS

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://easy-fix-ten.vercel.app",
     credentials: true,
     optionsSuccessStatus: 200,
   }),
