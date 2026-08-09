75844bff0ca70398ff254804489091727937091d
jamsheena <jamsheenalezin8@gmail.com>
2026-08-09 20:50:37 +0530

    nodemailer service

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -137,9 +137,7 @@ const userController = {
       await user.save();
 
       const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        service: "gmail",
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
